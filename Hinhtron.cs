864951f9c3b71f8a50e3ec42961f74eb2ed969b0
PhuongLinh301 <phuoglinh301@gmail.com>
2026-09-12 13:03:47 +0700

    Update Hinhtron.cs

--- Hinhtron.cs
+++ Hinhtron.cs
@@ -1,8 +1,24 @@
-﻿using System;
+using System;
 
-public class Class1
+namespace TinhToanHinhTron
 {
-	public Class1()
-	{
-	}
-}
+    public class HinhTron
+    {
+        public double BanKinh { get; set; }
+        public HinhTron(double banKinh)
+        {
+            BanKinh = banKinh;
+        }
+
+        //tính chu vi: C = 2 * PI * r
+        public double TinhChuVi()
+        {
+            return 2 * Math.PI * BanKinh;
+        }
+
+        //tính diện tích: S = PI * r^2
+        public double TinhDienTich()
+        {
+            return Math.PI * Math.Pow(BanKinh, 2);
+        }
+    }
