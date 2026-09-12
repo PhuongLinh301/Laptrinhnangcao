e6ea70759bf1a68b8f959e840aec9da07a8e0a60
PhuongLinh301 <phuoglinh301@gmail.com>
2026-09-12 10:13:10 +0700

    Add files via upload

---
+++ B1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace CongBaSo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            int c;
+
+            
+            Console.Write("Nhập a: ");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            
+            while (true)
+            {
+                try
+                {
+                    Console.Write("Nhập b: ");
+                    b = int.Parse(Console.ReadLine());
+                    break; 
+                }
+                catch
+                {
+                    Console.WriteLine("Nhập sai, vui lòng nhập lại!");
+                }
+            }
+
+            
+            bool k;
+            while (true)
+            {
+                Console.Write("Nhập c: ");
+                k = int.TryParse(Console.ReadLine(), out c);
+
+                if (k == true)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Nhập sai, vui lòng nhập lại!");
+                }
+            }
+
+        
+            int tong = a + b + c;
+            Console.WriteLine($"\nKết quả: {a} + {b} + {c} = {a + b + c}");
+
+            Console.ReadLine();
+        }
+    }
+}
+
